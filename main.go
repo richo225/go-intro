7266cc6a20c537eb9a4c0819b6af243f2b147d6c
Richard Bates <rich.bates@protonmail.com>
2023-04-20 17:57:12 +0800

    Refactor website check into own function

--- main.go
+++ main.go
@@ -16,13 +16,17 @@ var websites = []string{
 
 func main() {
 	for _, website := range websites {
-		resp, err := http.Get(website)
-
-		if err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-
-		fmt.Println(website, "status:", resp.Status)
+		checkWebsiteStatus(website)
 	}
+}
+
+func checkWebsiteStatus(website string) {
+	resp, err := http.Get(website)
+
+	if err != nil {
+		fmt.Println(website, "might be down: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(website, "status:", resp.Status)
 }
