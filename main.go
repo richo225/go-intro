d235e9ffe4cb28a6af5821fb04af13ccc6aa222f
Richard Bates <rich.bates@protonmail.com>
2023-04-20 17:35:02 +0800

    Print out status of each website

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+var websites = []string{
+	"http://google.com",
+	"http://facebook.com",
+	"http://stackoverflow.com",
+	"http://golang.org",
+	"http://amazon.com",
+}
+
+func main() {
+	for _, website := range websites {
+		resp, err := http.Get(website)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(website, "status:", resp.Status)
+	}
 }
