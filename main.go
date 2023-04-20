07b5bed9ed405b52c96bb2c373e45e513c0aa3ad
Richard Bates <rich.bates@protonmail.com>
2023-04-20 16:01:55 +0800

    Calculate area of square and triangle

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type triangle struct {
 	height float64
 	base   float64
@@ -9,5 +14,17 @@ type square struct {
 }
 
 func main() {
+	newSquare := square{sideLength: 5}
+	newTriangle := triangle{height: 5, base: 3}
 
+	fmt.Println("Area of square:", newSquare.getArea())
+	fmt.Println("Area of triangle:", newTriangle.getArea())
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return math.Pow(s.sideLength, 2)
 }
