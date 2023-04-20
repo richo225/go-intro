110ebdb280d5941510a77298165c1ae436ad087a
Richard Bates <rich.bates@protonmail.com>
2023-04-20 17:21:39 +0800

    Read test file from command line

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, file)
 }
